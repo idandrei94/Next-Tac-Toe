55de4eff81d3680af76fc45a1c3649e3f04c4e9a
Dan Andrei Iorga <idandrei94@gmail.com>
2022-02-13 22:34:52 +0100

    fixed import module typo

--- pages/api/sendMessage.ts
+++ pages/api/sendMessage.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import RoomEvents from 'common/RoomEvents';
+import RoomEvents from 'common/roomEvents';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import handleServerPusherRequest from 'server/handleServerPusherRequest';
 
